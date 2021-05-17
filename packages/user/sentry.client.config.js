e231e4967f1cfef76a63b14c8de00568b1252c66
Rojan <roseonmhr@gmail.com>
2021-05-17 18:54:05 +0545

    Add env example

--- packages/user/sentry.client.config.js
+++ packages/user/sentry.client.config.js
@@ -7,8 +7,6 @@ import * as Sentry from "@sentry/nextjs";
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn:
-    SENTRY_DSN ||
-    "https://4b065fa0b0fe4576808b6a740b80b6de@o567863.ingest.sentry.io/5763088",
-  environment: process.env.SENTRY_ENVIRONMENT,
+  dsn: SENTRY_DSN,
+  environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT,
 });
